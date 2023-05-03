843b8d6313cc2fabe268cabb050a2d80823232a7
florian <furnari.florian@hotmail.fr>
2023-05-03 13:30:15 +0200

    joined subclass strategy ok la difference avec single table c qu'il y a une table artist_item qui c'est crée

--- src/main/java/com/mycompany/mavenproject1/model/Item.java
+++ src/main/java/com/mycompany/mavenproject1/model/Item.java
@@ -20,7 +20,7 @@ import jakarta.persistence.Transient;
  * @author pc
  */
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "type")
 public abstract class Item {
     @Id
